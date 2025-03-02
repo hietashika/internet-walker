90bb80fbd7939d072a03b584a8bd54d8f0f424fd
hietashika <hietashika@gmail.com>
2025-03-02 23:05:43 +0900

    [style] 🎨 コードのフォーマットを整える - 各ファイルのインデントを統一し、ダブルクオートに変更しました。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import playformCompress from '@playform/compress';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), playformCompress()]
+	integrations: [mdx(), playformCompress()],
 });
